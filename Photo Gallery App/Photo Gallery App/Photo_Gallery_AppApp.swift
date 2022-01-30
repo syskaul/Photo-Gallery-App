da5acdf7f311059faf6475ae2174510f409f5a35
Eshaan Kaul <eshaankaul29@gmail.com>
2022-01-30 12:16:56 +0530

    Scroll View

---
+++ Photo Gallery App/Photo Gallery App/Photo_Gallery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Gallery_AppApp.swift
+//  Photo Gallery App
+//
+//  Created by Eshaan Kaul on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Gallery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo Gallery App/Photo Gallery AppUITests/Photo_Gallery_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Photo_Gallery_AppUITestsLaunchTests.swift
+//  Photo Gallery AppUITests
+//
+//  Created by Eshaan Kaul on 30/01/22.
+//
+
+import XCTest
+
+class Photo_Gallery_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
